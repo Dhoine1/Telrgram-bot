6965674eeead1618b8e7911218cb0f5361a0472b
Dhoine1 <136793336+Dhoine1@users.noreply.github.com>
2024-03-04 17:06:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot
-import configparser
-from datetime import date, datetime
+import sqlite3
+import datetime
+# import configparser
+# from datetime import date, datetime
 from extensions import APIException, ProcessingRequest
 from config import money, TOKEN
 
@@ -14,8 +16,6 @@ def send_help(message):
     Для перевода одной валюты в другую введите:\n
     <валюту, которую нужно перевести> <в какую нужно перевести> <количество> \n
     Например: usd rub 10\n
-    /enter - придти на работу\n
-    /leave - уйти с работы
     '''
     bot.reply_to(message, text)
 
@@ -29,51 +29,77 @@ def send_value(message):
     bot.reply_to(message, text)
 
 
-@bot.message_handler(commands=['enter'])
+@bot.message_handler(commands=['event'])
 def send_value(message):
-    try:
-        config = configparser.ConfigParser()
-        config.read('R:/OvrsrAgent.ini')
-        count = int(config.get('Overseer', 'EventOrdNo'))
-        config.set('Overseer', 'EventOrdNo', f'{count + 1}')
+    connection = sqlite3.connect('\\\\192.168.3.116\infosys\infosys\db.sqlite3')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM events_event')
+    events = cursor.fetchall()
 
-        with open('R:/OvrsrAgent.ini', 'w') as configfile:
-            config.write(configfile)
+    now_date = (datetime.datetime.now()+datetime.timedelta(days=1))
+    text_date = now_date.strftime("%d-%m-%Y")
+    text = f"Мероприятия на завтра: {text_date}"
 
-        with open(f'R:/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
-            f.write(f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},NTP,{count},5409041651212,ConfirmEnter,3972,Игнатьев Леонид,1\n')
+    for event in events:
+        if event[10] == now_date.strftime("%Y-%m-%d"):
+            sql = f"SELECT m.hall_name FROM events_halls as m, events_event as p, events_location as cp" \
+                  f" WHERE p.id = {event[0]} AND cp.event_id = p.id AND cp.hall_id = m.id"
+            cursor.execute(sql)
+            halls = cursor.fetchall()
+            hall = ""
+            for i in halls:
+                for j in i:
+                    hall += f'{j},  '
+            text += f'\n({event[3][:5]}-{event[4][:5]}) - {event[1]} \n Залы: {hall}\n-----'
+    connection.close()
+    bot.reply_to(message, text)
 
-        with open(f'G:/SHOP/Overseer/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
-            f.write(f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},NTP,{count},5409041651212,ConfirmEnter,3972,Игнатьев Леонид,1\n')
 
-        text = 'Вход выполнен'
-        bot.reply_to(message, text)
-    except Exception as e:
-        bot.reply_to(message, f"Произошла какая-то ошибка \n {e}")
+# @bot.message_handler(commands=['enter'])
+# def send_value(message):
+#     try:
+#         config = configparser.ConfigParser()
+#         config.read('R:/OvrsrAgent.ini')
+#         count = int(config.get('Overseer', 'EventOrdNo'))
+#         config.set('Overseer', 'EventOrdNo', f'{count + 1}')
+#
+#         with open('R:/OvrsrAgent.ini', 'w') as configfile:
+#             config.write(configfile)
+#
+#         with open(f'R:/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
+#             f.write(f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},RTC,{count},5409041651212,ConfirmEnter,4260,Игнатьев Леонид,108\n')
+#
+#         with open(f'G:/SHOP/Overseer/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
+#             f.write(f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},RTC,{count},5409041651212,ConfirmEnter,4260,Игнатьев Леонид,108\n')
+#
+#         text = 'Вход выполнен'
+#         bot.reply_to(message, text)
+#     except Exception as e:
+#         bot.reply_to(message, f"Произошла какая-то ошибка \n {e}")
 
 
-@bot.message_handler(commands=['leave'])
-def send_value(message):
-    try:
-        config = configparser.ConfigParser()
-        config.read('R:/OvrsrAgent.ini')
-        count = int(config.get('Overseer', 'EventOrdNo'))
-        config.set('Overseer', 'EventOrdNo', f'{count + 1}')
-
-        with open('R:/OvrsrAgent.ini', 'w') as configfile:
-            config.write(configfile)
-
-        with open(f'R:/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
-            f.write(f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},NTP,{count},5409041651212,ConfirmLeave,3972,Игнатьев Леонид,2\n')
-
-        with open(f'G:/SHOP/Overseer/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
-            f.write(
-                f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},NTP,{count},5409041651212,ConfirmLeave,3972,Игнатьев Леонид,2\n')
-
-        text = 'Выход выполнен'
-        bot.reply_to(message, text)
-    except Exception as e:
-        bot.reply_to(message, f"Произошла какая-то ошибка \n {e}")
+# @bot.message_handler(commands=['leave'])
+# def send_value(message):
+#     try:
+#         config = configparser.ConfigParser()
+#         config.read('R:/OvrsrAgent.ini')
+#         count = int(config.get('Overseer', 'EventOrdNo'))
+#         config.set('Overseer', 'EventOrdNo', f'{count + 1}')
+#
+#         with open('R:/OvrsrAgent.ini', 'w') as configfile:
+#             config.write(configfile)
+#
+#         with open(f'R:/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
+#             f.write(f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},RTC,{count},5409041651212,ConfirmLeave,4260,Игнатьев Леонид,107\n')
+#
+#         with open(f'G:/SHOP/Overseer/Ovrsr[{str(date.today()).replace("-", "")}].txt', 'a') as f:
+#             f.write(
+#                 f'{str(date.today()).replace("-", "")},{datetime.now().strftime("%H:%M:%S")},RTC,{count},5409041651212,ConfirmLeave,4260,Игнатьев Леонид,107\n')
+#
+#         text = 'Выход выполнен'
+#         bot.reply_to(message, text)
+#     except Exception as e:
+#         bot.reply_to(message, f"Произошла какая-то ошибка \n {e}")
 
 
 # Обработка присланного в чат текста
