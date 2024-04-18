d4ddab2fe705aad22b319b2c7b22b8d461e8e4ad
Dhoine1 <136793336+Dhoine1@users.noreply.github.com>
2024-04-18 16:46:16 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-TOKEN = "6252462535:AALHlJKLWHB9kVROfXLzxrcFLUKmFb5NgHuI"
+TOKEN = "6252462535:AAHlJKLWHB9kVROfXLzxrcFLUKmFb5NgHuI"
 
 money = {'Доллар США': 'USD',
          'Евро': 'EUR',
          'Рубль': 'RUB',
-         'Юань': 'CNY'}
+         'Юань': 'CNY',
+         'Узб. сум': 'UZS'}
